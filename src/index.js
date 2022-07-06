258e460f4e4a67a2198168c96da06e73001b2e01
unknown <kikridu@gmail.com>
2022-07-06 13:11:50 -0700

    page loading now

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
